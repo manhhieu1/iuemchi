ab333f3deb35ed983edae85e1f2dd851b6ac1199
Dinh Manh Hieu <manhhieua1@gmail.com>
2021-11-27 08:41:52 +0700

    sinh nhat thao

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const nameGirl = "Em yêu của Hiếu";
 // const giftUrl = 'http://nodemy.vn';
-const eventName = "Chúc Mừng 20-10";
+const eventName = "Chúc Mừng Sinh Nhật Em Người Yêu";
 const titleCard = "Hạ Thảo";
 const contentCard =
-  "<br/><br/>Gặp em trong đời là điều đúng đắn của đời anh. Em chính là nguồn động lực , là công tắc giúp anh vụt sáng. Yêu em là lựa chọn sáng suốt nhất KIKI. Chúc mình 20.10 vui vẻ , cinh đẹp cùng món quà tuyệt vời nhất là <span style='font-size:25px'>anh</span>.<br/> Anh yêu em. ";
+  "<br/><br/>Gặp em trong đời là điều đúng đắn của đời anh. Em chính là nguồn động lực , là công tắc giúp anh vụt sáng. Yêu em là lựa chọn sáng suốt nhất KIKI. Chúc mình sinh nhật vui vẻ , cinh đẹp cùng món quà tuyệt vời nhất là <span style='font-size:25px'>anh</span>.<br/> Anh yêu em. ";
 
 const giftImage = "38592f633fc1f69fafd0.jpg";
 const base64 = "";
